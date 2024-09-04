809fe9dc012b5b77498f3189e1d69f132c13be7f
aviralm52 <aviralm52@gmail.com>
2024-09-04 11:20:10 +0530

    resolved error in signup

--- src/app/authentication/signup/page.jsx
+++ src/app/authentication/signup/page.jsx
@@ -306,7 +306,7 @@ const PageSignUp = () => {
                   value={gender}
                   id="gender"
                   className="block w-full border-gray-600 focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 bg-white dark:border-neutral-700 dark:focus:ring-primary-6000 dark:focus:ring-opacity-25 dark:bg-neutral-900 rounded-2xl text-sm font-normal h-11 px-4 py-3 border"
-                  onChange={(e) => setGender(e.target.name)}
+                  onChange={(e) => setGender(e.target.value)}
                 >
                   <option value="Male">Male</option>
                   <option value="Female">Female</option>
