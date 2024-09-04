95e43789575415a756ba163fb7747e3946af1b51
ZairoDev <developer.zairo@gmail.com>
2024-09-04 10:51:31 +0530

    changes in table.jsx

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -92,114 +92,202 @@ export default function UserTable({ data, currentPage }) {
   };
 
   return (
-    // <TableContainer component={Paper}>
-    <table className={`w-full h-screen mt-4`}>
-      {/* <Table sx={{ minWidth: 650 }} aria-label="simple table"> */}
-      {/* <TableHead  className=" dark:bg-slate-900"> */}
-      <thead className=" dark:bg-slate-900">
-        <tr>
-          <th align="center" className=" font-bold text-xl">
-            S.No
-          </th>
-          <th align="center" className=" font-bold text-xl">
-            Name
-          </th>
-          <th align="center" className=" font-bold text-xl">
-            Email
-          </th>
-          <th align="center" className=" font-bold text-xl">
-            UserId
-          </th>
-          <th align="center" className=" font-bold text-xl">
-            Role
-          </th>
-          {/* <TableCell align="center" className=" font-bold text-xl">S.No</TableCell>
-            <TableCell align="center" className=" font-bold text-xl">Name</TableCell>
-            <TableCell align="center" className=" font-bold text-xl">Email</TableCell>
-            <TableCell align="center" className=" font-bold text-xl">UserId</TableCell>
-            <TableCell align="center" className=" font-bold text-xl">Role</TableCell> */}
-        </tr>
-      </thead>
-      <tbody>
-        {data?.map((user, index) => (
-          <tr
-            key={index}
-            sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-          >
-            {/* <TableCell align="center">{index+1}</TableCell> */}
-            <td align="center">{(currentPage - 1) * 20 + index + 1}</td>
-            <td component="th" scope="row" align="center">
-              {user.name}
-            </td>
-            <td align="center">{user.email}</td>
-            <td align="center">{user._id}</td>
-            <td align="center">{user.role}</td>
-            <td
-              align="center"
-              className=" hover:bg-gray-300  w-8 h-8 rounded-full cursor-pointer"
-              onClick={() =>
-                setExtra((prev) => {
-                  const newExtra = Array.from({ length: 20 }, () => false);
-                  newExtra[index] = !newExtra[index];
-                  return newExtra;
-                })
-              }
-            >
-              <div className=" relative">
-                <BsThreeDotsVertical className=" text-sm" />
-                {extra[index] && (
-                  <div
-                    ref={modalRef}
-                    className=" bg-white shadow-xl shadow-gray-600 dark:bg-slate-900 rounded-lg p-2 absolute right-8 top-4 transition-all ease-linear"
-                  >
-                    <Link href={`/users/userAccess/${user._id}`}>
-                      <div className=" flex gap-2 items-center cursor-pointer p-1">
-                        <FcCancel className=" text-xl" />
-                        <h2 className=" text-sm text-nowrap font-medium ">
-                          Ban User
-                        </h2>
-                      </div>
-                    </Link>
-                    <Link href={`/users/editUser/${user._id}`}>
-                      <div className=" flex gap-2 items-center cursor-pointer p-1">
-                        <HiPencilSquare className=" text-green-500" />
-                        <h2 className=" text-sm text-nowrap font-medium ">
-                          Edit details
-                        </h2>
-                      </div>
-                    </Link>
-                    <Link href={`/addproperties/${user._id}/${user.email}`}>
-                      {/* <Link
-                      href={{
-                        pathname: `/addproperties/${user._id}`,
-                        query: { email: user.email },
-                      }}
-                    > */}
-                      <div className=" flex gap-2 items-center cursor-pointer p-1">
-                        <FaPlusCircle className=" text-green-600" />
-                        <h2 className=" text-sm text-nowrap font-medium hover:text-gray-600">
-                          Add Property
-                        </h2>
-                      </div>
-                    </Link>
-                    <div
-                      className=" flex gap-2 items-center cursor-pointer p-1"
-                      onClick={() => handleDelete(user._id)}
-                    >
-                      <MdDeleteForever className=" text-red-600" />
-                      <h2 className=" text-sm text-nowrap font-medium hover:text-gray-600">
-                        Delete User
-                      </h2>
-                    </div>
-                  </div>
-                )}
-              </div>
-            </td>
+    <>
+      <table className={`w-full hidden md:block h-screen mt-4 `}>
+        <thead className=" dark:bg-slate-900">
+          <tr>
+            <th align="center" className=" font-bold text-xl">
+              S.No
+            </th>
+            <th align="center" className=" font-bold text-xl">
+              Name
+            </th>
+            <th align="center" className=" font-bold text-xl">
+              Email
+            </th>
+            <th align="center" className=" font-bold text-xl">
+              UserId
+            </th>
+            <th align="center" className=" font-bold text-xl">
+              Role
+            </th>
           </tr>
-        ))}
-      </tbody>
-      {deleteClick && deleteModal()}
-      {/* </Table> */}
-    </table>
+        </thead>
+        <tbody>
+          {data?.map((user, index) => (
+            <tr
+              key={index}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              {/* <TableCell align="center">{index+1}</TableCell> */}
+              <td align="center">{(currentPage - 1) * 20 + index + 1}</td>
+              <td component="th" scope="row" align="center">
+                {user.name}
+              </td>
+              <td align="center">{user.email}</td>
+              <td align="center">{user._id}</td>
+              <td align="center">{user.role}</td>
+              <td
+                align="center"
+                className=" hover:bg-gray-300  w-8 h-8 rounded-full cursor-pointer"
+                onClick={() =>
+                  setExtra((prev) => {
+                    const newExtra = Array.from({ length: 20 }, () => false);
+                    newExtra[index] = !newExtra[index];
+                    return newExtra;
+                  })
+                }
+              >
+                <div className=" relative">
+                  <BsThreeDotsVertical className=" text-sm" />
+                  {extra[index] && (
+                    <div
+                      ref={modalRef}
+                      className=" bg-white shadow-xl shadow-gray-600 dark:bg-slate-900 rounded-lg p-2 absolute right-8 top-4 transition-all ease-linear"
+                    >
+                      <Link href={`/users/userAccess/${user._id}`}>
+                        <div className=" flex gap-2 items-center cursor-pointer p-1">
+                          <FcCancel className=" text-xl" />
+                          <h2 className=" text-sm text-nowrap font-medium ">
+                            Ban User
+                          </h2>
+                        </div>
+                      </Link>
+                      <Link href={`/users/editUser/${user._id}`}>
+                        <div className=" flex gap-2 items-center cursor-pointer p-1">
+                          <HiPencilSquare className=" text-green-500" />
+                          <h2 className=" text-sm text-nowrap font-medium ">
+                            Edit details
+                          </h2>
+                        </div>
+                      </Link>
+                      <Link href={`/addproperties/${user._id}/${user.email}`}>
+                        <div className=" flex gap-2 items-center cursor-pointer p-1">
+                          <FaPlusCircle className=" text-green-600" />
+                          <h2 className=" text-sm text-nowrap font-medium hover:text-gray-600">
+                            Add Property
+                          </h2>
+                        </div>
+                      </Link>
+                      <div
+                        className=" flex gap-2 items-center cursor-pointer p-1"
+                        onClick={() => handleDelete(user._id)}
+                      >
+                        <MdDeleteForever className=" text-red-600" />
+                        <h2 className=" text-sm text-nowrap font-medium hover:text-gray-600">
+                          Delete User
+                        </h2>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        {deleteClick && deleteModal()}
+        {/* </Table> */}
+      </table>
+      {/* FOr the smaller screen */}
+
+      <table className={`w-full md:hidden h-screen mt-4 overflow-y-auto`}>
+        <thead className=" dark:bg-slate-900">
+          {/* <tr>
+            <th align="center" className=" font-bold text-xl">
+              S.No
+            </th>
+            <th align="center" className=" font-bold text-xl">
+              Name
+            </th>
+            <th align="center" className=" font-bold text-xl">
+              Email
+            </th>
+            <th align="center" className=" font-bold text-xl">
+              UserId
+            </th>
+            <th align="center" className=" font-bold text-xl">
+              Role
+            </th>
+          </tr>
+          */}
+        </thead>
+        <tbody>
+          {data?.map((user, index) => (
+            <tr
+              key={index}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              {/* <TableCell align="center">{index+1}</TableCell> */}
+              <td align="center">{(currentPage - 1) * 20 + index + 1}</td>
+              <td component="th" scope="row" align="center">
+                {user.name}
+              </td>
+              <td align="center">{user.email}</td>
+              <td align="center">{user._id}</td>
+              <td align="center">{user.role}</td>
+              <td
+                align="center"
+                className=" hover:bg-gray-300  w-8 h-8 rounded-full cursor-pointer"
+                onClick={() =>
+                  setExtra((prev) => {
+                    const newExtra = Array.from({ length: 20 }, () => false);
+                    newExtra[index] = !newExtra[index];
+                    return newExtra;
+                  })
+                }
+              >
+                {/* <div className=" relative">
+                  <BsThreeDotsVertical className=" text-sm" />
+                  {extra[index] && (
+                    <div
+                      ref={modalRef}
+                      className=" bg-white shadow-xl shadow-gray-600 dark:bg-slate-900 rounded-lg p-2 absolute right-8 top-4 transition-all ease-linear"
+                    >
+                      <Link href={`/users/userAccess/${user._id}`}>
+                        <div className=" flex gap-2 items-center cursor-pointer p-1">
+                          <FcCancel className=" text-xl" />
+                          <h2 className=" text-sm text-nowrap font-medium ">
+                            Ban User
+                          </h2>
+                        </div>
+                      </Link>
+                      <Link href={`/users/editUser/${user._id}`}>
+                        <div className=" flex gap-2 items-center cursor-pointer p-1">
+                          <HiPencilSquare className=" text-green-500" />
+                          <h2 className=" text-sm text-nowrap font-medium ">
+                            Edit details
+                          </h2>
+                        </div>
+                      </Link>
+                      <Link href={`/addproperties/${user._id}/${user.email}`}>
+                        <div className=" flex gap-2 items-center cursor-pointer p-1">
+                          <FaPlusCircle className=" text-green-600" />
+                          <h2 className=" text-sm text-nowrap font-medium hover:text-gray-600">
+                            Add Property
+                          </h2>
+                        </div>
+                      </Link>
+                      <div
+                        className=" flex gap-2 items-center cursor-pointer p-1"
+                        onClick={() => handleDelete(user._id)}
+                      >
+                        <MdDeleteForever className=" text-red-600" />
+                        <h2 className=" text-sm text-nowrap font-medium hover:text-gray-600">
+                          Delete User
+                        </h2>
+                      </div>
+                    </div>
+                  )}
+                </div> */}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        {deleteClick && deleteModal()}
+        {/* </Table> */}
+      </table>
+    </>
   );
 }
+
+// TODO :: Above code is working fine
